a558b2cc2da78e312718ba85effc84236555e939
AimanFadillah <111332318+AimanFadillah@users.noreply.github.com>
2024-05-23 18:27:45 +0700

    Update Data

--- src/data/Barangs.js
+++ src/data/Barangs.js
@@ -1,24 +1,29 @@
 const data = [
     {
-        nama:"Jeruk",
-        src:"https://keboncau-pamarayan.desa.id/asset/uploads/2021/09/istockphoto-1140677637-612x612-1.jpg",
-        harga:1500,
+        nama:"Magnum",
+        src:"",
+        harga:26000
     },
     {
-        nama:"Semangka",
-        src:"https://rsisurabaya.com/wp-content/uploads/2023/12/artikel-1.png",
-        harga:1500,
+        nama:"Gudang Garam",
+        src:"",
+        harga:27000
     },
     {
-        nama:"Anggur",
-        src:"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//112/MTA-64690797/no-brand_buah-anggur-red-globe-500gr_full01.jpg",
-        harga:1500,
+        nama:"Sampoerna Mild",
+        src:"",
+        harga:35000
     },
     {
-        nama:"Kelapa",
-        src:"https://res.cloudinary.com/dk0z4ums3/image/upload/v1682752500/attached_image/8-manfaat-buah-kelapa-untuk-kesehatan-yang-jarang-diketahui.jpg",
-        harga:1500,
+        nama:"Djarum Super",
+        src:"",
+        harga:26000
     },
+    {
+        nama:"Djarum Coklat",
+        src:"",
+        harga:17000
+    }
 ]
 
 export default data;
